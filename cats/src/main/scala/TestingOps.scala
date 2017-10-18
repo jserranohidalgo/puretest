5b1a84856e30fb0ec28eb0f176352b7e782ea366
Javier Fuentes <javier.fuentes@hablapps.com>
2017-10-18 10:59:10 +0200

    Added more testing ops.

--- cats/src/main/scala/TestingOps.scala
+++ cats/src/main/scala/TestingOps.scala
@@ -10,14 +10,20 @@ trait TestingOps {
 
   implicit class TestingOps[P[_], A](self: P[A]) {
 
+    def fails[E](implicit ME: ApplicativeError[P, E]): P[Boolean] =
+      (self as false) handleError { _ => true }
+
+    def succeeds[E](implicit ME: ApplicativeError[P, E]): P[Boolean] =
+      (self as true) handleError { _ => false }
+
     def isError[E: ApplicativeError[P, ?]](e: E): P[Boolean] =
       (self as false) handleError { _ == e }
 
-    def inspect[E: ApplicativeError[P, ?]]: P[Either[E, A]] =
-      (self map (Right(_): Either[E, A])).handleError(Left.apply[E, A])
-
     def isEqual(a: A)(implicit F: Functor[P]): P[Boolean] =
       self map (_ == a)
+
+    def inspect[E: ApplicativeError[P, ?]]: P[Either[E, A]] =
+      (self map (Right(_): Either[E, A])).handleError(Left.apply[E, A])
 
     def shouldFail(e: Throwable)(implicit ME: MonadError[P, Throwable],
         F: sourcecode.File, L: sourcecode.Line): P[Unit] =
