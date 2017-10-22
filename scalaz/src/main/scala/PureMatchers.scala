c401d7b382d813c802c9276759d38afbfa6b1c19
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2017-10-22 22:59:23 +0200

    remove annotations

--- scalaz/src/main/scala/PureMatchers.scala
+++ scalaz/src/main/scala/PureMatchers.scala
@@ -4,16 +4,15 @@ import scalaz.{Functor, Monad, MonadError}
 import scalaz.syntax.monadError._
 
 class PureMatchers[P[_], A](self: P[A])(implicit
-  M: Monad[P],
-  loc: Location){
+  M: Monad[P], loc: Location){
 
   // Basic
 
-  private def shouldFail[E](p: E => Boolean,
-    errorIfSuccess: A => PureTestError[E],
-    errorIfFailure: E => PureTestError[E])(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[Unit] =
+  private def shouldFail[E](p: E => Boolean)(
+      errorIfSuccess: A => PureTestError[E],
+      errorIfFailure: E => PureTestError[E])(implicit 
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[Unit] =
     HE.handleError(
       self.flatMap{ a: A => 
         RE.raiseError[Unit](errorIfSuccess(a))
@@ -23,11 +22,11 @@ class PureMatchers[P[_], A](self: P[A])(implicit
         else RE.raiseError(errorIfFailure(error))
     }
 
-  private def shouldSucceed[E](p: A => Boolean,
-    errorIfSuccess: A => PureTestError[E],
-    errorIfFailure: E => PureTestError[E])(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[A] =
+  private def shouldSucceed[E](p: A => Boolean)(
+      errorIfSuccess: A => PureTestError[E],
+      errorIfFailure: E => PureTestError[E])(implicit 
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[A] =
     HE.handleError(self){
       case error =>
         RE.raiseError[A](errorIfFailure(error))
@@ -39,34 +38,34 @@ class PureMatchers[P[_], A](self: P[A])(implicit
   // Failure
 
   def shouldMatchFailure[E](p: E => Boolean)(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[Unit] =
-    shouldFail[E](p, NotFailed[E,A](_), NotMatchedFailure[E](_))
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[Unit] =
+    shouldFail[E](p)(NotFailed(_), NotMatchedFailure(_))
 
   def shouldFail[E](implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[Unit] =
-    shouldFail[E]((_:E) => true, NotFailed[E,A](_), (_:E) => ShouldNotHappen[E]())
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[Unit] =
+    shouldFail[E]((_:E) => true)(NotFailed(_), _ => ShouldNotHappen())
 
   def shouldFail[E](e: E)(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[Unit] =
-    shouldFail[E]((_:E) == e, NotError[A,E](_:A, e), OtherError[E](_:E, e))
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[Unit] =
+    shouldFail[E]((_:E) == e)(NotError(_, e), OtherError(_, e))
 
   // Success
 
   def shouldMatch[E](p: A => Boolean)(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[A] =
-    shouldSucceed[E](p, NotMatched[A,E](_), NotSucceeded[E](_))
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[A] =
+    shouldSucceed[E](p)(NotMatched(_), NotSucceeded(_))
 
   def shouldBe[E](a: A)(implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[A] =
-    shouldSucceed[E]((_:A) == a, NotEqualTo[E,A](_:A, a), NotValue[A,E](_:E, a))
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[A] =
+    shouldSucceed[E]((_:A) == a)(NotEqualTo(_, a), NotValue(_, a))
 
   def shouldSucceed[E](implicit 
-  HE: HandleError[P, E],
-  RE: RaiseError[P, PureTestError[E]]): P[A] =
-    shouldSucceed[E]((_:A) => true, (_:A) => ShouldNotHappen[E](), NotSucceeded[E](_))
+      HE: HandleError[P, E],
+      RE: RaiseError[P, PureTestError[E]]): P[A] =
+    shouldSucceed[E]((_:A) => true)(_ => ShouldNotHappen(), NotSucceeded(_))
 }
